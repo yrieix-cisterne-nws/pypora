d6f3c89ce56202a1e89e78ab92475a41ff6c1f06
Yrieix-Cisterne-NWS <ycisterne@normandiewebschool.fr>
2026-05-18 21:49:54 +0200

    arborescence

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,25 @@
+export default function Dashboard() {
+    return(
+        <>
+            <div>
+                <p>Tableau de bord</p>
+                <p>Mes tutos</p>
+            </div>
+            <div>
+                <h1>Content de vous revoir, Yrieix</h1>
+            </div>
+            <div>
+                <h2>Continuez de regarder</h2>
+                <div>
+                    {/*tuto card */}
+                </div>
+            </div>
+            <div>
+                <h2>Vos certificats</h2>
+                <div>
+                    {/*certificat card */}
+                </div>
+            </div>
+        </>
+    )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,33 @@
 export default function Home() {
   return (
     <>
-    <p>Hello World!</p>
+      <div>
+        <div>
+          <h1>Bienvenue sur notre plateforme de tutoriels en ligne !</h1>
+          <p>
+            Découvrez une vaste collection de tutoriels couvrant divers sujets, allant de la programmation à la cuisine, en passant par le bricolage et bien plus encore. Que vous soyez débutant ou expert, notre plateforme offre des ressources adaptées à tous les niveaux.
+          </p>
+          <p>
+            Explorez nos catégories pour trouver des tutoriels qui correspondent à vos intérêts et commencez votre apprentissage dès aujourd'hui !
+          </p>
+        </div>
+        <div>
+          <img src="/public" alt="Image de bienvenue" />
+        </div>
+      </div>
+      <div>
+        <h2>Catégories</h2>
+      </div>
+      <div>
+        <h2>Tutoriels à la une</h2>
+        <p>Les sélections de nos experts pour booster vos compétences</p>
+      </div>
+      <div>
+        {/*tuto card */}
+        <div>
+          <p>Tout voir</p>
+        </div>
+      </div>
     </>
   );
 }
